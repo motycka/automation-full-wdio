6219b46629a2359ecc69c833fe1fa0600e07d821
monikaprotivova <monika.protivova@cleverfarm.ag>
2022-11-15 22:27:11 +0100

    lesson 7

--- specs/exercise.e2e.js
+++ specs/exercise.e2e.js
@@ -1,15 +1,17 @@
 import {username, password} from './fixtures.js'
+import LoginPage from '../pages/login.page'
+import ApplicationsPage from '../pages/applications.page'
 
 describe('Czechitas Login Page', () => {
 
     it('should open login page', () => {
-        
+
         browser.reloadSession();
-        
+
         browser.url('/prihlaseni');
 
         browser.pause(5000);
-        
+
     });
-    
+
 });
